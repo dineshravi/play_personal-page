75ffe8796566a88f467b3a3ebf573519171acd12
dineshravi <dineshraviiyer@gmail.com>
2013-04-07 13:01:48 -0700

    with-2pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,7 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("dineshra@buffalo.edu"));
     }
-  
+   public static Result tech(){
+	   return ok(tech.render());
+   }
 }
